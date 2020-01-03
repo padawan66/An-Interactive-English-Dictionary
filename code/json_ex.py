3707476ba441f7da0754810f7d4ceea9fb268f05
tejas.dongre@oracle.com <tejas.dongre@oracle.com>
2020-01-03 14:07:09 +0530

    removed local folder references

--- code/json_ex.py
+++ code/json_ex.py
@@ -1,4 +1,6 @@
 import json
-loaded_content = json.load(open("/Users/tejasdongre/Tejas/Personal/Python/Interactive_English_Dictionary/An-Interactive-English-Dictionary/data/data.json"))
+import os 
+dir_path = os.path.dirname(os.path.realpath("data.json"))
+loaded_content = json.load(open(dir_path.replace("/code","/data")+"/data.json"))
 print(type(loaded_content))
 print(loaded_content)
